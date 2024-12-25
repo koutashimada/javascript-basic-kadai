d64054cb07de7a74047ee8133330235cdc11cf05
koutashimada <ssssssssssssshkou@gmail.com>
2024-12-25 17:27:05 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,22 @@
+/*変数numが3の倍数の場合： “3の倍数です”
+変数numが5の倍数の場合： “5の倍数です”
+変数numが3と5の倍数の場合： “3と5の倍数です”
+それ以外の場合： 変数numの値を出力する*/
+
+let num=Math.floor(Math.random()* 20);
+console.log(num)
+if(num%3===0 && num%5===0){
+    console.log('3と5の倍数です');
+}
+
+else if(num%3===0){
+    console.log('3の倍数です');
+}
+
+else if(num%5===0){
+console.log('5の倍数です');
+}
+
+else{
+    console.log(num);
+}
