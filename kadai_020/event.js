5b59de13bb24ecf2f8b5b898364d2abac0ba3a53
koutashimada <ssssssssssssshkou@gmail.com>
2025-01-07 23:31:45 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const startbtn = document.getElementById('btn');
+const aftertext = document.getElementById('text');
+startbtn.addEventListener(('click'),() =>{
+    aftertext.textContent ='ボタンをクリックしました'
+});
